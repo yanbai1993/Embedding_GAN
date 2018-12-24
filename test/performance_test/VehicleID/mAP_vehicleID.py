2523c70f6d621f9ee93f35a5d1d432a10d9a7fa4
yichengh <anorange0409@gmail.com>
2018-12-24 20:31:45 +0800

    add vehicleID test list

--- test/performance_test/VehicleID/mAP_vehicleID.py
+++ test/performance_test/VehicleID/mAP_vehicleID.py
@@ -27,15 +27,15 @@ parser.add_argument('--image_dir', dest='image_dir',
                     default='', type=str)
 parser.add_argument('--repeat', dest='repeat',
                     help='repeat times',
-                    default=1, type=int)
+                    default=10, type=int)
 parser.add_argument('--maxg', dest='maxg',
                     help='max number of a class id in gallery',
                     default=1000, type=int)
 parser.add_argument('--save', dest='save',
                     help='save to file',
-                    default='map_vggm.txt', type=str)
+                    default='map_vggm_compress_2.txt', type=str)
 parser.add_argument('--save_dir', dest='save_dir',
-                    default='checkpoints/', type=str)
+                    default='checkpoints/car_cyclegan_VGGM/', type=str)
 parser.add_argument('--which_epoch', dest='which_epoch',
                     default='latest', type=str)
 parser.add_argument('--im_height', dest='im_height',
@@ -46,7 +46,7 @@ parser.add_argument('--im_width', dest='im_width',
                     default=224, type=int)
 args = parser.parse_args()
 
-def gen_gallery_probe_ori(samples, k=10000):
+def gen_gallery_probe(samples, k=1):
     """
     k: k samples for each id in gallery, 1 for reid
     TODO: generate gallery and probe sets.
@@ -54,7 +54,6 @@ def gen_gallery_probe_ori(samples, k=10000):
     """
     cls_ids = samples.keys()
     gallery = {}
-    extansion = {}
     probe = {}
     for cls_id in cls_ids:
         cls_samples = samples[cls_id]
@@ -62,7 +61,6 @@ def gen_gallery_probe_ori(samples, k=10000):
             continue
         gallery[cls_id] = []
         probe[cls_id] = []
-        extansion[cls_id] = []
         n = len(cls_samples)
         #  gid = np.random.randint(0, n)
         gids = np.random.permutation(np.arange(n))[:min(n-1, k)]
@@ -71,105 +69,9 @@ def gen_gallery_probe_ori(samples, k=10000):
                 gallery[cls_id].append(cls_samples[i])
             else:
                 probe[cls_id].append(cls_samples[i])
-                extansion[cls_id].append(cls_samples[i])
-    return gallery, extansion, probe
-
-def gen_gallery_probe_random(samples, k=1):
-    cls_ids = samples.keys()
-    gallery = {}
-    probe = {}
-    extansion = {}
-    for cls_id in cls_ids:
-        cls_samples = samples[cls_id]
-        n = len(samples[cls_id][0]) + len(samples[cls_id][1])
-        if n <=1:
-            continue
-        gallery[cls_id] = []
-        probe[cls_id] = []
-        extansion[cls_id] = []
-        pids = np.random.randint(0,n)
-        if(pids < len(samples[cls_id][0])):
-            pid_v = 0
-        else:
-            pid_v = 1
-            pids = pids - len(samples[cls_id][0])
-
-        exts = np.random.randint(0,n)
-        if(exts < len(samples[cls_id][0])):
-            ext_v = 0
-        else:
-            ext_v = 1
-            exts = exts - len(samples[cls_id][0])
-        
-        if(len(samples[cls_id][ext_v]) > 0):
-            exts = np.random.randint(0,len(samples[cls_id][ext_v]))
-            extansion[cls_id].append(cls_samples[ext_v][exts])
-        for i in xrange(len(cls_samples[0])):
-            if i == pids and pid_v == 0:
-                probe[cls_id].append(cls_samples[0][i])
-            else:
-                gallery[cls_id].append(cls_samples[0][i])
-
-        for i in xrange(len(cls_samples[1])):
-            if i == pids and  pid_v == 1:
-                probe[cls_id].append(cls_samples[1][i])
-            else:
-                gallery[cls_id].append(cls_samples[1][i])
-    return gallery, extansion, probe
-
-
-def gen_gallery_probe(samples, k=1):
-    cls_ids = samples.keys()
-    gallery = {}
-    probe = {}
-    extansion = {}
-    for cls_id in cls_ids:
-        cls_samples = samples[cls_id]
-        n = len(samples[cls_id][0]) + len(samples[cls_id][1])
-        if n <=1:
-            continue
-        gallery[cls_id] = []
-        probe[cls_id] = []
-        extansion[cls_id] = []
-        pids = np.random.randint(0,n)
-        if(pids < len(samples[cls_id][0])):
-            pid_v = 0
-        else:
-            pid_v = 1
-            pids = pids - len(samples[cls_id][0])
-        ext_v = (pid_v + 1)%2
-        
-        if(len(samples[cls_id][ext_v]) > 0):
-            exts = np.random.randint(0,len(samples[cls_id][ext_v]))
-            extansion[cls_id].append(cls_samples[ext_v][exts])
-        else:
-            extansion[cls_id].append(cls_samples[pid_v][pids])
-        for i in xrange(len(cls_samples[0])):
-            if i == pids and pid_v == 0:
-                probe[cls_id].append(cls_samples[0][i])
-            else:
-                gallery[cls_id].append(cls_samples[0][i])
-
-        for i in xrange(len(cls_samples[1])):
-            if i == pids and  pid_v == 1:
-                probe[cls_id].append(cls_samples[1][i])
-            else:
-                gallery[cls_id].append(cls_samples[1][i])
-    return gallery, extansion, probe
-
+    return gallery, probe
 
 def load_cls_samples(fname):
-    cls_samples = {}
-    for line in open(fname).readlines():
-        t = line.strip().split()
-        if int(t[1]) not in cls_samples:
-            cls_samples[int(t[1])] = {}
-            cls_samples[int(t[1])][0] = []
-            cls_samples[int(t[1])][1] = []
-        cls_samples[int(t[1])][int(t[2])%2].append(t[0])
-    return cls_samples
-
-def load_cls_samples_ori(fname):
     cls_samples = {}
     for line in open(fname).readlines():
         t = line.strip().split()
@@ -214,7 +116,7 @@ mean_avg_prec_e = np.zeros([args.repeat, ])
 f = open('result.txt','w')
 for r_id in xrange(args.repeat):
     cls_samples = load_cls_samples(args.list_file)
-    gallery, extansion, probe = gen_gallery_probe_random(cls_samples, args.maxg)
+    gallery, probe = gen_gallery_probe(cls_samples, args.maxg)
     if r_id==0:
         print 'Gallery size: %d' % (len(gallery.keys()))
     FEAT_SIZE = 1024 
@@ -224,6 +126,7 @@ for r_id in xrange(args.repeat):
         g_n += len(gallery[gid])
     for pid in probe:
         p_n += len(probe[pid])
+    #print g_n,p_n,e_n
     g_feat = np.zeros([g_n, FEAT_SIZE], dtype=np.float32)
     g_ids = np.zeros([g_n, ], dtype=np.float32)
     g_imgs = []
@@ -231,6 +134,7 @@ for r_id in xrange(args.repeat):
     for gid in gallery.keys():
         for s in gallery[gid]:
             img = Image.open(os.path.join(args.image_dir.strip(), s + ext)).convert('RGB')
+            #print img
             im = transformer(img)
             im = torch.unsqueeze(im, 0)
             im = im.cuda()
@@ -271,6 +175,9 @@ for r_id in xrange(args.repeat):
             map_p += (i+1)*1.0/(ind+1)
         map_p /= n
         mean_avg_prec_p[r_id] += map_p
+        #f.write(psample+' ' + str(pid) + '\n')
+        #for i in xrange(0,20):
+        #    f.write(p_sortimg[i]+' '+str(p_sorted[i])+'\n')
 
     mean_avg_prec_p[r_id] /= p_n
     print '============================= ITERATION %d =============================' % (r_id+1)
